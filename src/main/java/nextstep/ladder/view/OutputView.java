80ac62fb703b488f4bc907e0a23294fcde37a292
heesunjeong <jhswit@naver.com>
2020-08-30 00:02:28 +0900

    refactor: OutputView 상수 추가

--- src/main/java/nextstep/ladder/view/OutputView.java
+++ src/main/java/nextstep/ladder/view/OutputView.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class OutputView {
 
     public static final String RESULT_PRINT_MESSAGE = "\n실행결과\n";
+    public static final String LADDER_LINE_EMPTY = "     |";
+    public static final String LADDER_LINE_POINT = "-----|";
 
     private OutputView() {
     }
@@ -20,13 +22,17 @@ public class OutputView {
 
     public static void printLadder(Boolean[][] ladder) {
         for (Boolean[] line : ladder) {
-            StringBuilder sb = new StringBuilder("     |");
-
-            for (Boolean point : line) {
-                sb.append(point ? "-----|" : "     |");
-            }
-
-            System.out.println(sb);
+            printLadderLine(line);
         }
+    }
+
+    private static void printLadderLine(Boolean[] line) {
+        StringBuilder sb = new StringBuilder(LADDER_LINE_EMPTY);
+
+        for (Boolean point : line) {
+            sb.append(point ? LADDER_LINE_POINT : LADDER_LINE_EMPTY);
+        }
+
+        System.out.println(sb);
     }
 }
